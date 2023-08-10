ac25e44b3027dc7629f65cc76eb14dd3bbd43747
Daniel <danielrotheia@gmail.com>
2023-08-09 21:37:07 -0300

    remove csv

--- user/views.py
+++ user/views.py
@@ -2,21 +2,15 @@ from django.http import HttpResponse
 from .models import User
 import pandas as pd
 
-#Carrega um arquivo .ods, .xlsx, .xlx ou csv. Esse arquivo deve conter as colunas username, firstname, lastname, password e authority.
+#Carrega um arquivo .ods, .xlsx ou .xlx. Esse arquivo deve conter as colunas username, firstname, lastname, password e authority.
 #Cada linha representa um usuário. O username é um atributo único. 
 # Caso o arquivo seja recarregado ou possua 2 usuarios com o mesmo username, os objeto serão atualizado no banco
 def user_import(request):
     if request.method == 'POST':
         uploaded_file = request.FILES['file']
-        filename = uploaded_file.name
-        if '.csv' in filename:
-            df = pd.read_csv(uploaded_file)
-            df['authority'] = df['authority'].apply(lambda x: True if x == 'True' else False)
-        else: 
-            df = pd.read_excel(uploaded_file)
+        df = pd.read_excel(uploaded_file)
 
         df = df[['username', 'firstname', 'lastname', 'password', 'authority']]
-        #df['password'] = df['password'].apply(lambda x: str(uuid.uuid5(x))) #encriptar senha
         
         for user in df.itertuples():
             existing_user = User.objects.filter(username=user[1])
